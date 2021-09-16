a33c2acf43c7b8ebdc303c08f54a76b339ab0cb9
lochie <lochie.denholm@carsales.com.au>
2021-09-16 20:55:09 +1000

    implement graceful shutdown

--- product-service/go.mod
+++ product-service/go.mod
@@ -2,9 +2,6 @@ module github.com/ldenholm/systemsgo
 
 go 1.17
 
-require (
-	github.com/dave/jennifer v1.4.1 // indirect
-	github.com/devimteam/microgen v0.9.2 // indirect
-	github.com/fatih/structtag v1.2.0 // indirect
-	github.com/vetcher/go-astra v1.2.0 // indirect
-)
+require github.com/caarlos0/env v3.5.0+incompatible
+
+require github.com/stretchr/testify v1.7.0 // indirect
--- product-service/main.go
+++ product-service/main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ldenholm/systemsgo/handlers"
 )
 
 func main() {
+
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	helloHandler := handlers.NewHello(l)
 	goodbyeHandler := handlers.NewGoodbye(l)
 
-	// create servemux
+	// ServeMux
 	sm := http.NewServeMux()
+
+	// Assign Handlers
 	sm.Handle("/", helloHandler)
 	sm.Handle("/goodbye", goodbyeHandler)
-	http.ListenAndServe(":9090", sm)
+
+	// Create Server
+	server := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Signal Channel
+	// notify on os commands (interrupt & kill)
+	sigChannel := make(chan os.Signal)
+	signal.Notify(sigChannel, os.Interrupt)
+	signal.Notify(sigChannel, os.Kill)
+
+	sig := <-sigChannel
+	l.Println("Shutdown received, gracefully shutting down", sig)
+
+	timeout, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	// Graceful shutdown
+	server.Shutdown(timeout)
 }
